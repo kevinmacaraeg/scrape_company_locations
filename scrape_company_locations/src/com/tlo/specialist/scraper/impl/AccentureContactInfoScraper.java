94600bb7d70ca1d6eeb6cabd46fdbe04c5257ebf
Kevin Macaraeg <kevin.macaraeg@salesify.com>
2017-07-31 13:05:47 +0800

    07/31/2017 12:44PM

--- scrape_company_locations/src/com/tlo/specialist/scraper/impl/AccentureContactInfoScraper.java
+++ scrape_company_locations/src/com/tlo/specialist/scraper/impl/AccentureContactInfoScraper.java
@@ -1,4 +1,4 @@
-package com.tlo.specialist.service;
+package com.tlo.specialist.scraper.impl;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -16,16 +16,18 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.tlo.specialist.domain.CompanyContactInformation;
+import com.tlo.specialist.scraper.CompanyContactInfoScraper;
+import com.tlo.specialist.service.ScrapeCompanyContactInfoService;
 import com.tlo.specialist.util.Constants;
 import com.tlo.specialist.util.ExcelHelper;
 import com.tlo.specialist.util.FileHelper;
 import com.tlo.specialist.util.StringHelper;
 
-public class ScrapeAccentureContactInfoService {
+public class AccentureContactInfoScraper implements CompanyContactInfoScraper {
 	
-	private static Logger logger = Logger.getLogger(ScrapeAccentureContactInfoService.class.getName());
+	private static Logger logger = Logger.getLogger(AccentureContactInfoScraper.class.getName());
 	
-	private static final String ACCENTURE_OFFICE_DIRECTORY_LINK = "https://www.accenture.com/ph-en/office-directory";
+	private static final String ACCENTURE_OFFICE_DIRECTORY_URL = "https://www.accenture.com/ph-en/office-directory";
 	
 	private static final String[] ACCENTURE_SEARCH_LOCATION_STRINGS = {"Asia", "Middle East", "Africa", "Europe", "Americas"};
 	
@@ -43,15 +45,13 @@ public class ScrapeAccentureContactInfoService {
 	
 	private static final String ACCENTURE_PHONE_CSS_SELECTOR = "span.phone";
 
-	public void scrapeAccentureContactInfoToFile(String masterCompanyId, String masterCompanyName, String outputFilePath) throws Exception {
-		//Set<String> accentureFullCompanyContactInfoSet = null;
+	public void scrapeCompanyContactInformation(String masterCompanyId, String masterCompanyName, String outputFilePath) throws Exception {
 		List<CompanyContactInformation> companyContactInformationList = null;
 		try {
-			//accentureFullCompanyContactInfoSet = new HashSet<String>();
 			companyContactInformationList = new ArrayList<CompanyContactInformation>();
 			for (String searchLocationString : ACCENTURE_SEARCH_LOCATION_STRINGS) {
 				WebDriver driver = new ChromeDriver();
-				driver.get(ACCENTURE_OFFICE_DIRECTORY_LINK);
+				driver.get(ACCENTURE_OFFICE_DIRECTORY_URL);
 				
 				logger.info("Searching for " + searchLocationString + "...");
 				WebElement searchLocationBox = driver.findElement(By.id(ACCENTURE_OFFICE_DIRECTORY_SEARCH_BOX_ID));
@@ -67,7 +67,7 @@ public class ScrapeAccentureContactInfoService {
 			    	if (second >= 30) {
 			    		break;
 			    	}
-			    	jse.executeScript("window.scrollBy(0, Math.max(document.documentElement.scrollHeight,document.body.scrollHeight,document.documentElement.clientHeight));", ""); //y value '800' can be altered
+			    	jse.executeScript("window.scrollBy(0, Math.max(document.documentElement.scrollHeight,document.body.scrollHeight,document.documentElement.clientHeight));", "");
 			    	Thread.sleep(500);
 			    }
 			    
@@ -93,24 +93,18 @@ public class ScrapeAccentureContactInfoService {
 			    	CompanyContactInformation companyContactInfo = new CompanyContactInformation();
 			    	companyContactInfo.setMasterCompanyId(masterCompanyId);
 			    	companyContactInfo.setMasterCompanyName(masterCompanyName);
-			    	companyContactInfo.setCompanyLocationsUrl(ACCENTURE_OFFICE_DIRECTORY_LINK);
+			    	companyContactInfo.setCompanyLocationsUrl(ACCENTURE_OFFICE_DIRECTORY_URL);
 			    	companyContactInfo.setAddress(StringHelper.replaceNullValue(address, Constants.EMPTY_STRING).trim());
 			    	companyContactInfo.setPhoneNumber(StringHelper.replaceNullValue(phone, Constants.EMPTY_STRING).trim());
 			    	companyContactInfo.setFaxNumber(StringHelper.replaceNullValue(fax, Constants.EMPTY_STRING).trim());
 			    	
 			    	companyContactInformationList.add(companyContactInfo);
 			    	
-			    	//accentureFullCompanyContactInfoSet.add(individualDiv.text());
 			    }
 			    
 			    driver.quit();
 			}
 			
-//			companyContactInformationList = new ArrayList<CompanyContactInformation>();
-//
-//			List<CompanyContactInformation> parsedCompanyContactInformationList = ScrapeCompanyContactInfoService.parseEachContactInformation(ACCENTURE_OFFICE_DIRECTORY_LINK, accentureFullCompanyContactInfoSet);
-//			companyContactInformationList.addAll(parsedCompanyContactInformationList);
-//			
 			logger.info("Writing company contacts to output file...");
 			ScrapeCompanyContactInfoService service = new ScrapeCompanyContactInfoService();
 			String outputFileName = service.getOutputFileName(masterCompanyName);
